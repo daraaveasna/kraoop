0285c45854cb4cde4486549461d085ef27643cbf
TEANG Sreyroth <teangsreyroth80@gmail.com>
2024-02-07 22:19:22 +0700

    Orderpage

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomePage from "../views/HomePage.vue";
 import ProductPage from "../views/ProductPage.vue";
+import OrderPage from "../views/OrderPage.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path: "/", name: "home", component: HomePage},
     {path: "/product", name: "product", component: ProductPage},
+    {path: "/order", name:"order", component: OrderPage}
   ],
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
